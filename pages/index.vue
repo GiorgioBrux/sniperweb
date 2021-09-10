efb3f14c17c873c4acbafd33c3c31c94d64242f2
Giorgio <giorgiobrux2002@gmail.com>
2021-09-10 18:09:37 +0200

    feat: init nuxt.js

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card class="logo py-4 d-flex justify-center">
+        <DiscordLogo />
+      </v-card>
+      <v-card>
+        <v-card-title class="headline">
+          Welcome to the Vuetify + Nuxt.js template
+        </v-card-title>
+        <v-card-text>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
